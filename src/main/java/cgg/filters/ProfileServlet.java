9d4a3f55370168748464a5e1950f4951f1d4acfe
CGG <cggtsds59@gmail.com>
2023-08-01 17:19:20 +0530

    commmit 01-08-2023

---
+++ src/main/java/cgg/filters/ProfileServlet.java
@@ -0,0 +1,36 @@
+package cgg.filters;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ProfileServlet
+ */
+public class ProfileServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public ProfileServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Profile Servlet Executed...");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		out.println("<h1>Welcome to Profile Pages</h1>");
+		out.println("<h1>This is Profile Page</h1>");
+
+
+		
+	}
+
+}
